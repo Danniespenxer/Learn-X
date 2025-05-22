d4a91d908de75fd80c617641d71a4170ad9d26d7
Danniespenxer <92235455+Danniespenxer@users.noreply.github.com>
2025-05-23 00:08:59 +0100

    \

--- learn_x_full_stack/Frontend/src/components/StripePayment.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function StripePayment({ amount, onPay }: { amount: number; onPay: () => void }) {
-  return (
-    <div>
-      <h4>Pay ${amount / 100} for course access</h4>
-      <button onClick={onPay}>Pay with Stripe</button>
-    </div>
-  );
-}
